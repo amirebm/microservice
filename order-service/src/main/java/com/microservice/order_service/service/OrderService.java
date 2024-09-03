15997d7cd65df39547f169ed238958e750cb8116
amirebm <41531088+amirebm@users.noreply.github.com>
2024-09-03 22:59:13 +0200

    importe three services in one project

---
+++ order-service/src/main/java/com/microservice/order_service/service/OrderService.java
@@ -0,0 +1,79 @@
+package com.microservice.order_service.service;
+
+import com.microservice.order_service.dto.InventoryResponse;
+import com.microservice.order_service.dto.OrderLineItemsDto;
+import com.microservice.order_service.dto.OrderRequest;
+import com.microservice.order_service.model.Order;
+import com.microservice.order_service.model.OrderLineItems;
+import com.microservice.order_service.repository.OrderRepository;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+    public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemList(orderLineItems);
+
+
+        List<String> skuCodes = order.getOrderLineItemList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        //call inventory service and place order if the product is in stock
+//        Observation inventoryServiceObservation = Observation.createNotStarted("inventory-service-lookup",
+//                this.observationRegistry);
+//        inventoryServiceObservation.lowCardinalityKeyValue("call", "inventory-service");
+//        return inventoryServiceObservation.observe(() -> {
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        assert inventoryResponseArray != null;
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+
+        // publish Order Placed Event
+//     applicationEventPublisher.publishEvent(new OrderPlacedEvent(this, order.getOrderNumber()));
+//                return "Order Placed";
+    } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+
+       }
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
+        OrderLineItems orderLineItems=new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
